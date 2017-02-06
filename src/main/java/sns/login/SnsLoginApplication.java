ed28176a6ff44120b17a085a84266729eb76568c
mekappi <mekappi@gmail.com>
2017-02-06 20:02:50 +0900

    start project

---
+++ src/main/java/sns/login/SnsLoginApplication.java
@@ -0,0 +1,12 @@
+package sns.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnsLoginApplication.class, args);
+	}
+}
